b24976480a660ca456f41cb3de7d3c7bae96909f
cpiHostelcare <cpicstlab20@gamil.com>
2025-03-05 13:05:00 +0600

    update code

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+
+function App() {
+
+  return (
+    <>
+    <h1>Our flag</h1>
+     <div className="bg-green-800 h-50 w-70 flex justify-center items-center">
+     <div className="bg-red-800 h-25 w-25 rounded-full">
+
+    </div>
+     </div>
+     <div className="h-300 w-3 bg-amber-900">
+
+     </div>
+    </>
+  )
+}
+
+export default App
